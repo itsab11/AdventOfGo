14f062af602515666c2b1fcfc798850196803eb5
Itsab11 <sebastian.schleicher@gmail.com>
2024-12-06 08:32:54 +0100

    day1 part1

---
+++ day1-1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"cmp"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+)
+
+func main() {
+	var left, right []int
+	inputfile, err := os.Open("input")
+	if err != nil {
+		fmt.Println(err)
+	}
+	scanner := bufio.NewScanner(inputfile)
+	scanner.Split(bufio.ScanWords)
+	var isLeft = true
+	for scanner.Scan() {
+		if isLeft {
+			value, _ := strconv.Atoi(scanner.Text())
+			left = append(left, value)
+			isLeft = false // for the next run
+		} else {
+			value, _ := strconv.Atoi(scanner.Text())
+			right = append(right, value)
+			isLeft = true // so it alternates between left and right
+		}
+	}
+	slices.SortFunc(left, func(a, b int) int {
+		return cmp.Compare(a, b)
+	})
+	slices.SortFunc(right, func(a, b int) int {
+		return cmp.Compare(a, b)
+	})
+	total_difference := 0
+	for i := range left {
+		diff := right[i] - left[i]
+		if diff < 0 {
+			diff = diff * -1
+		}
+		total_difference += diff
+	}
+
+	fmt.Printf("Total difference: %v \n", total_difference)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/itsab11/AdventOfGo
+
+go 1.23.4
